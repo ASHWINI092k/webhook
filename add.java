305868bf8efa32675cc4e60cab20ca7b6125359e
ashwini-09 <ashwini09@gmail.com>
2025-10-04 20:04:47 +0530

    Initial commit on add.java

---
+++ add.java
@@ -0,0 +1,19 @@
+public class AdditionProgram {
+
+    // This function takes two integers as input and returns their sum
+    public static int addNumbers(int num1, int num2) {
+        int sum = num1 + num2;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int number1 = 10;
+        int number2 = 25;
+
+        // Call the addNumbers function and store the result
+        int result = addNumbers(number1, number2);
+
+        // Print the result
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + result);
+    }
+}
